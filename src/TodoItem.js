ed07cf051cabc86feb3c129ab12cadeeca54d9b7
Oliver Gomes <ogomesphoto@gmail.com>
2019-01-17 17:10:20 -0500

    props handleChange

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -6,7 +6,7 @@ const TodoItem = props => {
       <input
         type="checkbox"
         checked={props.item.completed}
-        onChange={() => console.log("clicked!")}
+        onClick={() => props.handleChange(props.item.id)}
       />
       <p>{props.item.text}</p>
     </div>
