900846fa3dbecee900d01e52da87dcec1f39abd2
Oliver Gomes <ogomesphoto@gmail.com>
2019-01-17 13:58:17 -0500

    import data and map throught to project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
 import TodoItem from "./TodoItem";
-
+import todosData from "./todosData";
 function App() {
-  return (
-    <div className="todo-list">
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-    </div>
-  );
+  const data = todosData.map(todo => {
+    return <TodoItem text={todo.text} />;
+  });
+
+  return <div className="todo-list">{data}</div>;
 }
 
 export default App;
