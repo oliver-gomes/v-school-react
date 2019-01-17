c22d3627fbb8cf7c3d779693c35a859117c29394
Oliver Gomes <ogomesphoto@gmail.com>
2019-01-17 16:35:10 -0500

    onChange added

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,8 +3,12 @@ import React from "react";
 const TodoItem = props => {
   return (
     <div className="todo-item">
-      <input type="checkbox" />
-      <p>{props.text}</p>
+      <input
+        type="checkbox"
+        checked={props.item.completed}
+        onChange={() => console.log("clicked!")}
+      />
+      <p>{props.item.text}</p>
     </div>
   );
 };
