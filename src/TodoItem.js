2c27c5e3f53956924e0bbeef66a795982aef61ee
Oliver Gomes <ogomesphoto@gmail.com>
2019-01-17 21:19:09 -0500

    style change when complete task

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,6 +1,12 @@
 import React from "react";
 
 const TodoItem = props => {
+  const completedStyle = {
+    fontStyle: "italic",
+    color: "#cdcdcd",
+    textDecoration: "line-through"
+  };
+
   return (
     <div className="todo-item">
       <input
@@ -8,7 +14,9 @@ const TodoItem = props => {
         checked={props.item.completed}
         onClick={() => props.handleChange(props.item.id)}
       />
-      <p>{props.item.text}</p>
+      <p style={props.item.completed ? completedStyle : null}>
+        {props.item.text}
+      </p>
     </div>
   );
 };
