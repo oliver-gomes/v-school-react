c4d222fe28f0544b117951759482a15d106d2b2b
Oliver Gomes <ogomesphoto@gmail.com>
2019-01-18 19:18:49 -0500

    Meme generator finish touch

--- meme-generator/src/MemeGenerator.js
+++ meme-generator/src/MemeGenerator.js
@@ -4,24 +4,62 @@ class MemeGenerator extends Component {
   state = {
     topText: "",
     bottomText: "",
-    randomImg: "http://i.imgflip.com/1bij.jpg"
+    randomImg: "http://i.imgflip.com/1bij.jpg",
+    allMemeImgs: []
   };
 
   componentDidMount() {
     fetch("https://api.imgflip.com/get_memes")
       .then(response => response.json())
-      .then(data =>
+      .then(response => {
+        const { memes } = response.data;
         this.setState({
-          topText: data.data.memes[0].name
-        })
-      );
+          allMemeImgs: memes
+        });
+      });
   }
 
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const randNum = Math.floor(Math.random() * this.state.allMemeImgs.length);
+    const randMemeImg = this.state.allMemeImgs[randNum].url;
+    this.setState({
+      randomImg: randMemeImg
+    });
+  };
   render() {
     return (
       <div>
-        <h1>Meme Generator Section</h1>
-        <p>{this.state.topText}</p>
+        <form className="meme-form" onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="topText"
+            placeholder="Top text"
+            value={this.state.topText}
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            name="bottomText"
+            placeholder="Bottom text"
+            value={this.state.bottomText}
+            onChange={this.handleChange}
+          />
+          <button>Gen</button>
+        </form>
+
+        <div className="meme">
+          <img src={this.state.randomImg} alt="" />
+          <h2 className="top">{this.state.topText}</h2>
+          <h2 className="bottom">{this.state.bottomText}</h2>
+        </div>
       </div>
     );
   }
